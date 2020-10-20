f4827c20b702a5b446610b4b9bc9d8d190efaec9
withColinSong <71534090+withColinSong@users.noreply.github.com>
2020-10-20 16:35:29 +0900

    Example06

---
+++ 03.Academy/01.Java/03.JavaGenerics/Generics06.java
@@ -0,0 +1,31 @@
+package Day20.generics08;
+
+class Box<T> {
+	private T ob;
+	public void set(T o) {
+		ob = o;
+	}
+	
+	public T get() {
+		return ob;
+	}
+}
+
+class BoxFactory {
+	public static <T>Box<T> makeBox(T o){
+		Box<T> box = new Box<T>();
+		box.set(o);
+		return box;
+	}
+}
+public class GenericMethodBoxMaker {
+
+	public static void main(String[] args) {
+		Box<String> sBox = BoxFactory.makeBox("Sweet");
+		System.out.println(sBox.get());
+		
+		Box<Double> dBox = BoxFactory.makeBox(7.59);
+		System.out.println(dBox.get());
+	}
+
+}

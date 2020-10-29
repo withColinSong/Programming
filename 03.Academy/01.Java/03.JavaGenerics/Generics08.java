0bf37f7e62b98957d37e23a4673d9ba1fba94586
withColinSong <71534090+withColinSong@users.noreply.github.com>
2020-10-29 22:18:35 +0900

    HashMapEx2

---
+++ 03.Academy/01.Java/03.JavaGenerics/Generics08.java
@@ -0,0 +1,24 @@
+package Day19;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class HashMapEx2 {
+	public static void main(String[] args) {
+		HashMap map = new HashMap();
+		map.put("김자바", 90);
+		map.put("김자바", 100);
+		map.put("이자바", 100);
+		
+		Set set = map.entrySet();
+		Iterator it = set.iterator();
+		
+		while(it.hasNext()) {
+			Map.Entry e = (Map.Entry)it.next();
+			System.out.println("이름 : " + e.getKey() + ", 점수 : " + e.getValue());
+		}
+	}
+	
+}

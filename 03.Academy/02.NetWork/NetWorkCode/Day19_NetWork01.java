05b6141d40b1f57dc5b582dc22caf668900c24e8
withColinSong <71534090+withColinSong@users.noreply.github.com>
2020-10-29 22:09:16 +0900

    InetAddressEx

--- 03.Academy/02.NetWork/NetWorkCode/Day19_NetWork01.java
+++ 03.Academy/02.NetWork/NetWorkCode/Day19_NetWork01.java
@@ -1,36 +1,43 @@
   package Day19;
   //20.10.19
 
-  import java.net.InetAddress;
-  import java.net.UnknownHostException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
 
-  public class InetAddressEx {
-    InetAddress ip = null;
-    String ip1 = null;
+public class InetAddressEx {
+	
+	public void testInetAddress(String domain) {
+		InetAddress ip = null;
+		try {
+			ip = InetAddress.getByName(domain);
+			System.out.println("HostName : " + ip.getHostName());
+			System.out.println("HostAddress : " + ip.getHostAddress());
+			System.out.println("HostName/HostAddress : \n+\t" + ip.toString());
+			System.out.println("-----------------------");
+			
+			byte[] ipAddr = ip.getAddress();
+			System.out.println("getAddress() : " + Arrays.toString(ipAddr));
+			System.out.println("HostAddress : ");
+			
+			for(int i=0; i < ipAddr.length; i++) {
+				if(ipAddr[i] < 0) {
+					System.out.print(ipAddr[i] + 256);
+				}else {
+					System.out.println(ipAddr[i]);
+				}
+				System.out.print(".");
+			}
+			
+			System.out.println("");
+			System.out.println("------------------------");
+			ip = InetAddress.getLocalHost();
+			System.out.println("LocalHostName : " + ip.getHostName());
+			System.out.println("LocalHostAddress() : " + ip.getHostAddress());
+			System.out.println("Local HostName/HostAddress : \n\t" + ip.toString());
+		}catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+	}
+}
 
-    public void testInetAddress() throws UnknownHostException  { //throw 던져주다
-      //오류를 처리해주는 조상 Exception
-      ip = InetAddress.getLocalHost(); //IP주소 얻어온다.	
-      System.out.println(ip); //로컬 호스트 주소
-      ip1 = ip.getHostAddress();
-      System.out.println(ip1); //호스트 주소
-
-      System.out.println(ip.getHostName());
-      System.out.println(ip.getHostAddress());
-      System.out.println(ip.toString());
-    }
-
-
-    @Override
-    public String toString() { 
-  //		return "InetAddressEx [ip=" + ip + ", ip1=" + ip1 + "]";
-      return "ip주소 확인하는 중 ...."; //굳이 ip주소를 알려주지 않아도 될 때
-    }
-
-    public static void main(String[] args) throws UnknownHostException{
-  //		InetAddressEx inet = new InetAddressEx().testInetAddress; //1번 방법
-  //		inet.testInetAddress();
-  //		System.out.println(inet.toString());
-      new InetAddressEx().testInetAddress(); //2번 방법 생성자.메소드
-    }
-  }

5f0653fddce1440b6a44214f386bde2dc3f398c1
withColinSong <71534090+withColinSong@users.noreply.github.com>
2020-10-29 22:14:11 +0900

    Echo_test_main

---
+++ 03.Academy/02.NetWork/NetWorkCode/Day19_NetWork07.java
@@ -0,0 +1,9 @@
+package Day19;
+
+  import Day19.TCPEchoServer;
+  public class Echo_test_main {
+
+    public static void main(String[] args) {
+      new TCPEchoServer().tcpSerever(9007);
+    }
+  }

7b80c15cfa47b54ae4f2ff37a7304b0982bc5e03
withColinSong <71534090+withColinSong@users.noreply.github.com>
2020-11-04 15:30:36 +0900

    02.thread

---
+++ 03.Academy/01.Java/06.JavaThread/BeepThread.java
@@ -0,0 +1,21 @@
+import java.awt.Toolkit;
+
+
+public class BeepThread extends Thread {
+
+	@Override
+	public void run() {
+		Toolkit tk = Toolkit.getDefaultToolkit();
+
+		while(true) {
+			tk.beep();
+			try {
+				System.out.printf("[%s] thread is running\n", Thread.currentThread().getName());
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+

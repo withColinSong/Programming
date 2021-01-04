b9cde5c5998ade8c51c7a7ee24cabff9f4736bdb
withColinSong <withcolinsong@gmail.com>
2021-01-04 10:52:39 +0900

    ALTER TABLE boardAtt RENAME COLUMN board_serial TO pserial;

--- 03.Academy/06.kh-web-2020/MyBatis/20.12.24/20.12.24.sql
+++ 03.Academy/06.kh-web-2020/MyBatis/20.12.24/20.12.24.sql
@@ -19,14 +19,17 @@ CREATE TABLE boardAtt(
     sysFile VARCHAR2(200)
 );
 
+ALTER TABLE boardAtt RENAME COLUMN board_serial TO pserial;
 /*borad, boardAtt 시퀀스 */
 
 CREATE SEQUENCE seq_board;
 CREATE SEQUENCE seq_boardAtt;
 
+
 /* 조회 */
 SELECT * FROM board;
 SELECT * FROM boardAtt;
+
 
 INSERT INTO board VALUES(SEQ_BOARD.nextval, 'song', '1234', 'Java', '', SYSDATE, 1, 1);
 INSERT INTO board VALUES(SEQ_BOARD.nextval, 'song', '1234', 'Java', '', SYSDATE, 2, 2);
@@ -38,5 +41,6 @@ INSERT INTO boardAtt VALUES(SEQ_BOARDATT.nextval, 1, '', '');
 INSERT INTO boardAtt VALUES(SEQ_BOARDATT.nextval, 2, '', '');
 INSERT INTO boardAtt VALUES(SEQ_BOARDATT.nextval, 3, '', '');
 INSERT INTO boardAtt VALUES(SEQ_BOARDATT.nextval, 4, '', '');
+
 
 commit

286cb21c274e9abd678b4367f6da6df6f664d8f4
withColinSong <withcolinsong@gmail.com>
2021-01-14 23:06:49 +0900

    modify servlet

--- 01.TIL/05.FE/Ajax/ajax-json-servlet-gson/ajax/src/com/ajax/searchKH.java
+++ 01.TIL/05.FE/Ajax/ajax-json-servlet-gson/ajax/src/com/ajax/searchKH.java
@@ -15,31 +15,7 @@ import com.google.gson.Gson;
 @WebServlet("/ajax/searchKH")
 public class searchKH extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 
-		request.setCharacterEncoding("utf-8");
-		response.setCharacterEncoding("utf-8");
-		response.setContentType("text/html; charset=utf-8");
-		
-		PrintWriter out = response.getWriter();
-		
-		String findStr = "당산지원";
-		
-		System.out.println("findStr : " + findStr);
-		
-		MemberDao dao = new MemberDao();
-		
-		Kh kh = dao.select(findStr);
-
-		System.out.println("dao : " + dao.select(findStr));
-		Gson gson = new Gson();
-		String result = gson.toJson(kh);
-		out.print(result);
-		out.close();
-		System.out.println(result);
-	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
